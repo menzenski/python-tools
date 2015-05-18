ba2e23b9f74fd203a3fc635c4a3dc8c25f1ae7a9
Matt Menzenski <matt.menzenski@gmail.com>
2015-05-18 17:03:18 -0500

    updated gitignore

---
+++ ebook-extractor/ebookextractor.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##########
+## textsplitter.py Version 1.0 (2015-03-23)
+##
+## Original author: Matthew Menzenski (menzenski@ku.edu)
+##
+## License: CC-BY-4.0 ( https://creativecommons.org/licenses/by/4.0/ )
+##########
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
