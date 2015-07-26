f7cb88d24f3704116b0a8aa552629123aa64fbd8
Matt Menzenski <matt.menzenski@gmail.com>
2015-07-26 16:32:23 -0500

    scrape token and source information from ancient subcorpus

--- rnc-historical-searcher/rnchistoricalsearcher.py
+++ rnc-historical-searcher/rnchistoricalsearcher.py
@@ -38,6 +38,19 @@
 from urllib import FancyURLopener
 from bs4 import BeautifulSoup as Soup
 import re
+import urlparse
+
+## from: http://stackoverflow.com/questions/4389572/how-to-fetch-a-non-ascii-url-with-python-urlopen
+#def url_encode_non_ascii(b):
+#    return re.sub('[\x80-\xFF]', lambda c: '%%%02x' % ord(c.group(0)), b)
+#
+#def iri_to_url(iri):
+#    parts = urlparse.urlparse(iri)
+#    return urlparse.urlunparse(
+#        part.encode('idna') if parti==1 else url_encode_non_ascii(
+#            part.encode('utf-8'))
+#        for parti, part in enumerate(parts)
+#    )
 
 class RNCSource(object):
     """One source in RNC search results."""
@@ -49,6 +62,7 @@ class RNCSource(object):
         self.date_begin = 0   # earliest date given
         self.date_middle = 0  # middle of date range
         self.date_end = 0     # latest date given
+        self.name = re.sub(r'\([^)]*\)', '', self.source)
 
     def dates(self):
         """Return source dates as numeric values (float or int)."""
@@ -88,15 +102,15 @@ class RNCSource(object):
                 self.date_end = date_year
 
         ## if date is given outside parentheses (less common)
-    elif re.findall('\d{4}', self.source):
-        date_year = int(re.findall('\d{4}', self.source))
-        self.date_begin = date_year
-        self.date_middle = date_year
-        self.date_end = date_year
+        elif re.findall('\d{4}', self.source):
+            date_year = int(re.findall('\d{4}', self.source))
+            self.date_begin = date_year
+            self.date_middle = date_year
+            self.date_end = date_year
 
-    ## TODO: decide if this else statement can just be scrapped altogether
-    else:
-        print "No date found"
+        ## TODO: decide if this else statement can just be scrapped altogether
+        else:
+            print "No date found"
 
 
 class MyOpener(FancyURLopener):
@@ -121,10 +135,17 @@ class Webpage(object):
         myopener = MyOpener()
         self.html = myopener.open(self.address).read()
         self.soup = Soup(self.html)
+        #try:
+        #    self.html = myopener.open(self.address).read()
+        #    self.soup = Soup(self.html)
+        #except UnicodeError:
+        #    self.html = myopener.open(iri_to_url(self.address)).read()
+        #    self.soup = Soup(self.html)
 
 
 class RNCSearchTerm(object):
     """Object containing all (past-tense) forms of a search term."""
+    ## this really just provides a namespace for organizing search terms.
 
     def __init__(self):
         # ancient corpus needs lemmas and grammatical forms
@@ -143,6 +164,83 @@ class RNCSearchTerm(object):
         self.modern_splx_ipf = []
         self.old_pfx_pf = []
         self.old_pfx_ipf = []
+
+class RNCWord(object):
+    """One word in RNC search results (any subcorpus), with grammatical information."""
+
+    def __init__(self, span):
+        """Initialize RNCWord object."""
+
+        pass
+
+class RNCWordExplainAncient(object):
+    """Pop-up explanation page for a word in the ancient RNC subcorpus."""
+
+    def __init__(self, mode="old_rus", sort="gr_created", lang="ru",
+                 doc="", parent1=0, level1=0, lexi1="", gramm1="",
+                 parent2=0, level2=0, min2=1, max2=1, text="word-info",
+                 requestid=1437941630686, language="ru", source=""):
+        """Initialize with empty search parameters."""
+
+        self.mode = mode
+        self.sort = sort
+        self.lang = lang
+        self.doc = doc
+        self.parent1 = parent1
+        self.level1 = level1
+        self.lexi1 = lexi1
+        self.gramm1 = gramm1
+        self.parent2 = parent2
+        self.level2 = level2
+        self.min2 = min2
+        self.max2 = max2
+        self.text = text
+        self.requestid = requestid
+        self.language = language
+        self.source = source
+
+        self.params = {
+            "mode": self.mode,
+            "sort": self.sort,
+            "lang": self.lang,
+            "doc": self.doc,
+            "parent1": self.parent1,
+            "level1": self.level1,
+            "lexi1": self.lexi1,
+            "gramm1": self.gramm1,
+            "parent2": self.parent2,
+            "level2": self.level2,
+            "min2": self.min2,
+            "max2": self.max2,
+            "text": self.text,
+            "requestid": self.requestid,
+            "language": self.language,
+            "source": self.source,
+            }
+
+    def explain_url_ancient(self):
+        """Generate explaination url from parameters."""
+
+        self.address = u"http://search-beta.ruscorpora.ru/search-explain.xml?"
+
+        for k, v in self.params.iteritems():
+            self.address += u"{}={}&".format(k, v)
+
+        return self.address
+
+    def scrape_explain_ancient(self):
+        """Get grammatical and semantic info for a word in ancient subcorpus."""
+
+        self.explain_url_ancient()
+
+        page = Webpage(self.address)
+
+        self.surface_form = page.soup.find('span', attrs={"class": "cs"}).string
+        self.lemma = self.surface_form.find_next('td', attrs={"class": "value"}).string
+
+        self.surface_form = unicode(self.surface_form)
+        self.lemma = unicode(self.lemma)
+
 
 
 class RNCSearchAncient(object):
@@ -181,7 +279,9 @@ class RNCSearchAncient(object):
             "max2": self.max2,
             }
 
-    def search_url(self):
+        self.results_page_urls = []
+
+    def base_search_url(self):
         """Generate a search url from parameters."""
 
         self.address = "http://search-beta.ruscorpora.ru/search.xml?"
@@ -191,27 +291,105 @@ class RNCSearchAncient(object):
 
         return self.address
 
-    def search_ancient(self):
-        """Return search results from the 'ancient' subcorpus of the RNC."""
+    def addresses_ancient(self):
+        """Return list of results page urls from a search of 'ancient' RNC."""
 
         page_idx = 0
-        while True:
-            self.address += "p={}&".format(page_idx)
-            page = RNCSearchAbstract(self.address)
+        self.base_search_url()
+        self.address = self.address + "p={}&".format(page_idx)
+        while 1:
+            ## TODO: change this line so it doesn't keep adding the same thing
+            #new_string = re.sub(r'"(\d+),(\d+)"', r'\1.\2', original_string)
+            self.address = re.sub(r'p=(\d+?)&', 'p={}&'.format(
+                page_idx), self.address)
 
-            page_idx += 1
+            #self.address += "p={}&".format(page_idx)
+            page = Webpage(self.address)
+
+            try:
+                if page.soup.ol.findAll('li'):
+                    page_idx += 1
+                    self.results_page_urls.append(self.address)
+                    #print "Hey! {}\n\n".format(self.address)
+            except AttributeError:
+                break
+            else:
+                break
+
+    def scrape_one_page_ancient(self, url):
+        """Return source, date, and token for one page of results from 'ancient' RNC."""
+
+        page = Webpage(url)
+        sources_on_page = []
+        for itm in page.soup.ol.findAll('li'):
+            sources = itm.find_all('span', attrs={'class': 'b-doc-expl'})
+            for source in sources:
+                src = RNCSource(source.string)
+                print src.name
+
+                source_list = source.find_next('ul')
+                words = source_list.find_all('span', attrs={'class': 'b-wrd-expl g-em cs'})
+                for word in words:
+                    #print u"{};{}".format(word.string, word.get('explain'))
+                    #
+                    #expl = RNCWordExplainAncient(
+                    #    lexi1=word.string, source=word.get('explain')
+                    #    )
+                    #
+                    #expl.explain_url_ancient()
+                    #print "{}\n\n".format(expl.address)
+                    print self.lexi1
+                    print self.gramm1
+                    print word.string
+
 
 
 class RNCSearchOld(object):
     """Search of the 'old' subcorpus of the Russian National Corpus."""
 
-    def __init__(self, search_term):
-        self.splx_ipf = search_term.old_splx_ipf
-        self.pfx_pf = search_term.old_pfx_pf
-        self.pfx_ipf = search_term.old_pfx_ipf
+    def __init__(self):
+        pass
 
-    def search_old(self):
-        """Return search results from the 'old' subcorpus of the RNC."""
+#    def base_search_url(self):
+#        """Generate a search url from parameters."""
+#
+#        self.address = "http://search-beta.ruscorpora.ru/search.xml?"
+#
+#        for k, v in self.params.iteritems():
+#            self.address += "{}={}&".format(k, v)
+#
+#        return self.address
+
+    def base_search_url():
+        pass
+
+    def addresses_old(self):
+        """Return list of results page urls from a search of 'old' RNC."""
+
+        page_idx = 0
+        self.base_search_url()
+        self.address = self.address + "p={}&".format(page_idx)
+        while 1:
+            ## TODO: change this line so it doesn't keep adding the same thing
+            #new_string = re.sub(r'"(\d+),(\d+)"', r'\1.\2', original_string)
+            self.address = re.sub(r'p=(\d+?)&', 'p={}&'.format(
+                page_idx), self.address)
+
+            #self.address += "p={}&".format(page_idx)
+            page = Webpage(self.address)
+
+            try:
+                if page.soup.ol.findAll('li'):
+                    page_idx += 1
+                    self.results_page_urls.append(self.address)
+                    #print "Hey! {}\n\n".format(self.address)
+            except AttributeError:
+                break
+            else:
+                break
+
+    def scrape_one_page_old(self, url):
+        """Return source, date, and token for one page of results from 'old' RNC."""
 
         pass
 
@@ -291,7 +469,7 @@ class RNCSearchModern(object):
             #"sem-mod2": ,
             }
 
-    def search_url(self):
+    def base_search_url(self):
         """Generate a search url from parameters."""
 
         self.address = "http://search.ruscorpora.ru/search.xml?"
@@ -301,8 +479,33 @@ class RNCSearchModern(object):
 
         return self.address
 
-    def search_modern(self):
-        """Return search results from the 'modern' subcorpus of the RNC."""
+    def addresses_modern(self):
+        """Return list of results page urls from a search of 'modern' RNC."""
+
+        page_idx = 0
+        self.base_search_url()
+        self.address = self.address + "p={}&".format(page_idx)
+        while 1:
+            ## TODO: change this line so it doesn't keep adding the same thing
+            #new_string = re.sub(r'"(\d+),(\d+)"', r'\1.\2', original_string)
+            self.address = re.sub(r'p=(\d+?)&', 'p={}&'.format(
+                page_idx), self.address)
+
+            #self.address += "p={}&".format(page_idx)
+            page = Webpage(self.address)
+
+            try:
+                if page.soup.ol.findAll('li'):
+                    page_idx += 1
+                    self.results_page_urls.append(self.address)
+                    #print "Hey! {}\n\n".format(self.address)
+            except AttributeError:
+                break
+            else:
+                break
+
+    def scrape_one_page_modern(self, url):
+        """Return source, date, and token for one page of results from 'modern' RNC."""
 
         pass
 
@@ -322,18 +525,56 @@ def main():
     ubrati.modern_pfx_pf = ["убрать"]
     ubrati.modern_pfx_ipf = ["убирать"]
 
-    for lemma in ubrati.ancient_splx_ipf:
-        for form in ubrati.ancient_forms:
-            search = RNCSearchAncient(lexi1=lemma,gramm1=form)
-            search.search_url()
-            print search.address
+    sobrati = RNCSearchTerm()
+    # lemmas for the ancient subcorpus
+    sobrati.ancient_splx_ipf = ["брати", "бьрати"]
+    sobrati.ancient_pfx_pf = ["събрати", "събьрати", "собрати", "собьрати"]
+    sobrati.ancient_pfx_ipf = ["събирати", "собирати"]
+    # word forms for the old subcorpus
+    sobrati.old_splx_ipf = [""]
+    sobrati.old_pfx_pf = [""]
+    sobrati.old_pfx_ipf = [""]
+    # lemmas for the modern subcorpus
+    sobrati.modern_splx_ipf = ["брать"]
+    sobrati.modern_pfx_pf = ["собрать"]
+    sobrati.modern_pfx_ipf = ["собирать"]
 
-    for lemma in ubrati.modern_splx_ipf:
-        for form in ubrati.modern_forms:
-            print "{},{}".format(lemma, form)
-            search = RNCSearchModern(lex1=lemma,gramm1=form)
-            search.search_url()
-            print search.address
+    for lemma in sobrati.ancient_splx_ipf:
+        for form in sobrati.ancient_forms:
+            search = RNCSearchAncient(lexi1=lemma,gramm1=form)
+            search.base_search_url()
+            search.addresses_ancient()
+
+            for url in search.results_page_urls:
+                print "{}\n\n".format(url)
+                search.scrape_one_page_ancient(url)
+
+    for lemma in sobrati.ancient_pfx_pf:
+        for form in sobrati.ancient_forms:
+            search = RNCSearchAncient(lexi1=lemma,gramm1=form)
+            search.base_search_url()
+            search.addresses_ancient()
+
+            for url in search.results_page_urls:
+                print "{}\n\n".format(url)
+                search.scrape_one_page_ancient(url)
+
+    for lemma in sobrati.ancient_pfx_ipf:
+        for form in sobrati.ancient_forms:
+            search = RNCSearchAncient(lexi1=lemma,gramm1=form)
+            search.base_search_url()
+            search.addresses_ancient()
+
+            for url in search.results_page_urls:
+                print "{}\n\n".format(url)
+                search.scrape_one_page_ancient(url)
+
+#    for lemma in ubrati.modern_splx_ipf:
+#        for form in ubrati.modern_forms:
+#            print "{},{}".format(lemma, form)
+#            search = RNCSearchModern(lex1=lemma,gramm1=form)
+#            search.base_search_url()
+#            print search.address
 
 
 if __name__ == "__main__":
